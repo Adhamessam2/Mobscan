94a53abf736332858b9652062930e2f12edcdbb3
salma-y <salmayasser677@gmail.com>
2026-07-02 05:25:03 +0300

    edits

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:mobscan/core/appcolors.dart';
 import 'package:mobscan/screens/Permissions_Screen.dart';
 import 'package:mobscan/screens/Setting_Screen.dart';
+import 'package:mobscan/screens/admin_blacklist_screen.dart';
 import 'package:mobscan/screens/apps.dart';
+import 'package:mobscan/screens/link_checker_screen.dart';
 import 'package:mobscan/screens/main_dashboard.dart';
 
 class HomePage extends StatefulWidget {
@@ -18,6 +20,8 @@ class _HomePageState extends State<HomePage> {
   final List<Widget> pages = [
     MainDashboard(),
     Apps(),
+    LinkCheckerScreen(),
+    //AdminBlacklistScreen(),
     PermissionsScreen(),
     SettingsScreen(),
   ];
@@ -46,6 +50,14 @@ class _HomePageState extends State<HomePage> {
 
           BottomNavigationBarItem(
             icon: Icon(Icons.apps_rounded, color: Appcolors.cardColor),
+            label: '',
+           ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(Icons.app_blocking,color: Appcolors.cardColor),
+          //   label: '',
+          // ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.link, color: Appcolors.cardColor),
             label: '',
           ),
           BottomNavigationBarItem(
